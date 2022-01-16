fd68dbee79b59e3773ec0024b1d7810144e135e0
Xinyi Wang <xinyiw56@gmail.com>
2022-01-16 00:55:50 -0500

    added unit conversion

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 $('document').ready(function () {
     $('.add_another').click(function () {
-        $("#tbl").append('<tr><td id = "rowTag" > Ingredient:</td><form action=""><td><input id="numberToConvert" type="number"></td><td><div><select name="originalUnit" id="originalUnit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></div></td><td><div><input name="ingredient" id="ingredient" type="text" ></div> </td><td>convert to <select name="convertedUnit" id="convertedUnit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></td></form></tr>');
+        $("#tbl").append('<tr><td id = "rowTag" > Ingredient:</td><form action="">\
+        <td><input id="numberToConvert" type="number"></td>\
+        <td><div><select name="originalUnit" id="originalUnit">\
+        <option value="N/A">N/A</option>\
+        <option value="mL">mL</option>\
+        <option value="L">L</option>\
+        <option value="oz">oz</option>\
+        <option value="g">g</option>\
+        <option value="kg">kg</option>\
+        </select></div></td><td>\
+        <div><input name="ingredient" id="ingredient" type="text" ></div> \
+        </td><td>convert to <select name="convertedUnit" id="convertedUnit">\
+        <option value="N/A">N/A</option>\
+        <option value="mL">mL</option>\
+        <option value="L">L</option>\
+        <option value="oz">oz</option>\
+        <option value="g">g</option>\
+        <option value="kg">kg</option>\
+        </select></td></form></tr>');
     });
 })
 
-
+const ozInG = 28.35;
+const ozInMl = 29.57;
 
 const addForm = document.forms['ingredient'];
 
@@ -23,14 +42,28 @@ addForm.addEventListener('submit', function (e) {
         console.log(row.querySelector('input[name="ingredient"]').value);
         console.log(row.querySelector('select[id="convertedUnit"]').value);
 
-
+        const preUni = row.querySelector('select[id="originalUnit"]').value;
+        const targetUni = row.querySelector('select[id="convertedUnit"]').value;
         var quantity = row.querySelector('input[type="number"]').value;
         const ingredient = row.querySelector('input[name="ingredient"]').value;
 
-        quantity = Math.round(quantity * ratio*100)/100;
+        //convert unit
+        quantity = convertUnit(preUni, targetUni, quantity);
+
+        //convert quantity
+        quantity = Math.round(quantity * ratio * 100) / 100;
 
         const newRow = document.createElement('tr');
-        newRow.append(quantity + " " + ingredient);
+
+        if (targetUni == 'N/A' && preUni == 'N/A') {
+            newRow.append(quantity + " " + ingredient);
+        } else if (targetUni == 'N/A') {
+            newRow.append(quantity + " " + preUni + " " + ingredient);
+        }
+        else {
+            newRow.append(quantity + " " + targetUni + " " + ingredient);
+        }
+
         table.insertAdjacentElement('beforeend', newRow);
     }
 
@@ -41,4 +74,73 @@ addForm.addEventListener('submit', function (e) {
 
 function deleteResult() {
     document.querySelector("#resultTable").innerHTML = "";
+}
+
+function convertUnit(preUnit, targetUni, quantity) {
+    let weights = ["g", "kg", "oz"];
+    let volumes = ['L', 'mL', 'oz'];
+
+    switch (targetUni) {
+        case 'g':
+            if (!weights.includes(preUnit)) {
+                throw Error("Invalid unit conversion!");
+            } else if (preUnit == 'g') {
+                return quantity;
+            } else if (preUnit == 'kg') {
+                return quantity * 1000;
+            } else if (preUnit == 'oz') {
+                return quantity * ozInG;
+            }
+        case 'kg':
+            if (!weights.includes(preUnit)) {
+                throw Error("Invalid unit conversion!");
+            } else if (preUnit == 'g') {
+                return quantity / 1000;
+            } else if (preUnit == 'kg') {
+                return quantity;
+            } else if (preUnit == 'oz') {
+                return quantity * ozInG / 1000;
+            }
+        case 'oz':
+            if (preUnit == 'N/A') {
+                throw Error("Invalid unit conversion!");
+            } else if (preUnit == 'g') {
+                return quantity / ozInG;
+            } else if (preUnit == 'kg') {
+                return quantity * 1000 / ozInG;
+            } else if (preUnit == 'oz') {
+                return quantity;
+            } else if (preUnit == 'L') {
+                return (quantity * 1000 / ozInMl);
+            } else if (preUnit == 'mL') {
+                return (quantity / ozInMl);
+            } else if (preUnit == 'oz') {
+                return quantity;
+            }
+        case 'L':
+            if (!volumes.includes(preUnit)) {
+                throw Error("Invalid unit conversion!");
+            } else if (preUnit == 'L') {
+                return quantity;
+            } else if (preUnit == 'mL') {
+                return quantity / 1000;
+            } else if (preUnit == 'oz') {
+                return quantity * ozInMl / 1000;
+            }
+        case 'mL':
+            if (!volumes.includes(preUnit)) {
+                throw Error("Invalid unit conversion!");
+            } else if (preUnit == 'L') {
+                return quantity * 1000;
+            } else if (preUnit == 'mL') {
+                return quantity;
+            } else if (preUnit == 'oz') {
+                return quantity * ozInMl;
+            }
+        case 'N/A':
+            if (preUnit != 'N/A') {
+                throw Error("Invalid unit conversion!");
+            }
+            return quantity;
+    }
 }
