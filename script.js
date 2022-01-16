c3c7d24436c6f827155acffaaa0b785562cb46de
Hazel Jiang <shuaishuai.jiang01@gmail.com>
2022-01-15 20:09:10 -0500

    added some new feature(eg. add a new line)

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const number = document.querySelector('.numberToConvert');
-const convertFrom = document.querySelector('.convertFrom');
-const convertTo = document.querySelector('.convertTo');
+$('document').ready(function () {
+    $('.add_another').click(function () {
+        $("#tbl").append('<tr><td id = "rowTag" > Ingredient:</td><form action=""><td><input id="numberToConvert" type="number" placeholder="6"></td><td><div><select name="unit" id="unit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></div></td><td><div><input id="ingredient" type="text" placeholder="eggs"></div> </td><td>convert to <select name="unit" id="unit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></td></form></tr>');
+    });
+})
 
-function handleInput(number, convertFrom, convertTo) {
-    if (convertFrom == 'Grams' && convertTo == 'Kilograms') {
-        var result = document.querySelector('.result');
-        result.setAttribute(number / 1000);
-    }
-}
+
