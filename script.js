3954f7477dbfe155982d9b7074eb55bdb93424c9
Hazel Jiang <shuaishuai.jiang01@gmail.com>
2022-01-15 21:31:31 -0500

    minor changes

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $('document').ready(function () {
     $('.add_another').click(function () {
-        $("#tbl").append('<tr><td id = "rowTag" > Ingredient:</td><form action=""><td><input id="numberToConvert" type="number" placeholder="6"></td><td><div><select name="unit" id="unit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></div></td><td><div><input id="ingredient" type="text" placeholder="eggs"></div> </td><td>convert to <select name="unit" id="unit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></td></form></tr>');
+        $("#tbl").append('<tr><td id = "rowTag" > Ingredient:</td><form action=""><td><input class="data" id="numberToConvert" type="number"></td><td><div><select name="unit" id="unit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></div></td><td><div><input class="data" id="ingredient" type="text" ></div> </td><td>convert to <select name="unit" id="unit"><option value="N/A">N/A</option><option value="mL">mL</option><option value="L">L</option></select></td></form></tr>');
     });
 })
 
 
+
+const addForm = document.forms['ingredient'];
+
+addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const rows = addForm.querySelectorAll('tr');
+    for (i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        console.log(row.querySelector('input[type="number"]').value);
+        console.log(row.querySelector('select[id="originalUnit"]').value);
+        console.log(row.querySelector('input[name="ingredient"]').value);
+        console.log(row.querySelector('select[id="convertedUnit"]').value);
+    }
+
+});
